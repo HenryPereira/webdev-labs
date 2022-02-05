99de296c906496e829f7e8122349d1b9c7e4854a
HenryPereira <56375668+HenryPereira@users.noreply.github.com>
2022-02-04 21:09:04 -0600

    finished lab05

--- lab05/03-dyslexia-mode/index.js
+++ lab05/03-dyslexia-mode/index.js
@@ -1,4 +1,16 @@
 /* 
   See Smashing Magazine Tutorial:
   https://www.smashingmagazine.com/2021/11/dyslexia-friendly-mode-website/
+  -
 */
+
+const dyslexiaMode = () => {
+  const body = document.querySelector("body");
+  if(body.className === "") {
+    body.className = "dyslexia-mode";
+  } else {
+    body.className = "";
+  }
+}
+
+document.querySelector("#dyslexia-toggle").addEventListener('click', dyslexiaMode);
